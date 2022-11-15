d81b16694d119fbd6a9a728fa44409b66638faa6
olfa-ayari99 <olfa.ayari@sprit.tn>
2022-11-15 18:28:22 +0100

    added backgournd video

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EvenementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     #[Route('/index', name: 'app_index')]
-    public function index(): Response
+    public function index(EvenementRepository $evenementRepository): Response
     {
+        // dd($evenementRepository->findBy( [] , [] , 3)[0]->getSponsor());
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'events' => $evenementRepository->findBy( [] , [] , 3),
         ]);
     }
 }
